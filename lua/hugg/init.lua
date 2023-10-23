62b114559ac5396d70b8416a859fa1ec99c41b0f
hugo <hugo.gomez9795@gmail.com>
2023-10-23 22:25:46 +0200

    First commit

---
+++ lua/hugg/init.lua
@@ -0,0 +1,31 @@
+require("hugg.remap")
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
+
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
---
+++ lua/hugg/remap.lua
@@ -0,0 +1,28 @@
+vim.keymap.set("n", "<leader>ez", vim.cmd.Ex)
+
+-- open a terminal
+vim.keymap.set("n", "<leader>tm", vim.cmd.terminal)
+
+-- remove highlighting from last word search
+vim.keymap.set("n", "<leader>rh", vim.cmd.nohlsearch)
+
+-- delete current buffer
+vim.keymap.set("n", "<C-w>", vim.cmd.bdelete)
+
+-- Alt up and down equivalent
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- C-c to be equivalent of escape
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+-- Disable Q
+vim.keymap.set("n", "Q", "<nop>")
+
+-- Equivalent of Ctrl+Shift+l in vscode
+-- change current word in everyplace in the file
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+
+-- keymaps from old habits
+vim.keymap.set("n", "<C-s>", vim.cmd.w)
